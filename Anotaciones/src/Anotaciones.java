87dd5823f140951f2faee6cb98cb032d625a7842
Federico 'Magus' Rocca <frocca17@gmail.com>
2020-08-16 15:32:10 -0300

    Agregado de anotaciones de listas enlazadas.

---
+++ Anotaciones/src/Anotaciones.java
@@ -0,0 +1,17 @@
+
+public class Anotaciones
+{
+    
+    /*
+     * Los arraylist almacenan la información de manera contigua. Esto quiere decir que al momento de agregar un
+     * elemento al arraylist se tiene que volver a alocal todo el vector si el mismo no tiene espacio, por lo que
+     * en ocasiones es lento. Lo mismo se aplica al eliminar elementos, excepto el primero y el último.
+     * En cambio para leer es mucho mas rápido.
+     *
+     * Los Linkedlist es una lista enlazada normal. La lectura es lenta al igual que el eliminado, pero el agregado es
+     * rápido.
+     *
+     *
+     */
+    
+}
